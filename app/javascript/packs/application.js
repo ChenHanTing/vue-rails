fb28b47d63ffd40f2d8a9652ea93b3c551356aba
Han <han@jvdiamondtech.com>
2020-03-21 07:41:50 +0800

    complete todolists

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,19 +14,19 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import Vue from 'vue'
-import App from '../todolists.vue'
-import VueResource from 'vue-resource'
+import Vue from "vue";
+import App from "../todolists.vue";
+import VueResource from "vue-resource";
 
 Vue.use(VueResource);
 
-document.addEventListener('DOMContentLoaded', () => {
-  Vue.http.headers.common['X-CSRF-Token'] = document.getElementsByName('csrf-token')[0].getAttribute('content')
-  const el = "#todolists"
+document.addEventListener("DOMContentLoaded", () => {
+  Vue.http.headers.common["X-CSRF-Token"] = document
+    .getElementsByName("csrf-token")[0]
+    .getAttribute("content");
+  const el = "#todolists";
   const app = new Vue({
     el,
     render: h => h(App)
-  })
-
-  console.log(app)
-})
+  });
+});
--- app/javascript/todolists.vue
+++ app/javascript/todolists.vue
@@ -1,26 +1,41 @@
-# app/javascript/todolists.vue
 <template>
   <div>
     <h1>Todo Lists</h1>
 
-    <input type="text" v-model="todo" class="form-control" autofocus="true">
-    <button @click="addTodo()" class="btn btn-primary" :disabled="!todo.length">Add Todo</button>
+    <input type="text" v-model="todo" class="form-control" autofocus="true" />
+    <button @click="addTodo()" class="btn btn-primary" :disabled="!todo.length">
+      Add Todo
+    </button>
 
     <table>
       <thead>
         <tr>
           <th>#</th>
           <th>Item</th>
-           <th>operate</th>
+          <th>operate</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr v-for="todo in list" >
+        <tr v-for="todo in list">
           <td>{{ todo.id }}</td>
-          <td>{{ todo.item }}</td>
           <td>
-            <button @click="deleteTodo(todo.id)" class="btn btn-primary">Delete Todo</button>
+            <span @click="changeTodo(todo)" v-show="todo.editable">
+              {{ todo.item }}</span
+            >
+            <input v-show="!todo.editable" v-model="todo.item" />
+            <button
+              v-show="!todo.editable"
+              @click="updateTodo(todo)"
+              class="btn btn-primary"
+            >
+              Update Todo
+            </button>
+          </td>
+          <td>
+            <button @click="deleteTodo(todo.id)" class="btn btn-primary">
+              Delete Todo
+            </button>
           </td>
         </tr>
       </tbody>
@@ -29,40 +44,50 @@
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        todo: '',
-        list: [
-          { id: 1, item: "Foo" },
-          { id: 2, item: "Bar" }
-        ]
-      }
-    },
-
-  created: function() {
-   this.fetchTodoLists();
+export default {
+  data: function() {
+    return {
+      todo: "",
+      list: [
+        { id: 1, item: "Foo" },
+        { id: 2, item: "Bar" }
+      ]
+    };
   },
-
+  created: function() {
+    this.fetchTodoLists();
+  },
   methods: {
-    addTodo(){
-      this.$http.post('todolists.json', { item: this.todo }, {})
-      .then((res) => this.fetchTodoLists(), this.todo = '')
-      .catch((error) => console.log('Got a problem' + error));
+    addTodo() {
+      this.$http
+        .post("todolists.json", { item: this.todo }, {})
+        .then(res => this.fetchTodoLists(), (this.todo = ""))
+        .catch(error => console.log("Got a problem" + error));
     },
-
-    deleteTodo(todo_id){
-      this.$http.delete('todolists/'+ todo_id +'.json')
-      .then((res) => this.fetchTodoLists())
-      .catch((error) => console.log('Got a problem' + error));
+    deleteTodo(todo_id) {
+      this.$http
+        .delete("todolists/" + todo_id + ".json")
+        .then(res => this.fetchTodoLists())
+        .catch(error => console.log("Got a problem" + error));
     },
-
+    changeTodo(todoItem) {
+      todoItem.editable = !todoItem.editable;
+    },
+    updateTodo(todoItem) {
+      this.$http
+        .put("todolists/" + todoItem.id, { item: todoItem.item }, {})
+        .then(res => this.fetchTodoLists())
+        .catch(error => console.log("Got a problem" + error));
+    },
     fetchTodoLists: function() {
-       const resource = this.$resource('/todolists.json/{ id }');
-       resource.get().then(function(response){
-         this.list = response.data
-       });
-      }
+      const resource = this.$resource("/todolists.json/{ id }");
+      resource.get().then(function(response) {
+        const newList = response.data.map(oldItem => {
+          return { ...oldItem, editable: true };
+        });
+        this.list = newList;
+      });
     }
   }
+};
 </script>
